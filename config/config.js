a4dd702a7f59de1b3cb0d712c8aacdb47d8e42d1
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-01-23 12:44:10 +0900

    commit

--- config/config.js
+++ config/config.js
@@ -1,13 +1,32 @@
 require("dotenv").config();
 
 module.exports = {
+  // development: {
+  //   username: process.env.DB_USERNAME, // .env에서 불러오기
+  //   password: process.env.DB_PASSWORD, // .env에서 불러오기
+  //   database: process.env.DB_DATABASE, // .env에서 불러오기
+  //   host: process.env.DB_HOST, // .env에서 불러오기
+  //   dialect: process.env.DB_DIALECT, // .env에서 불러오기 (예: 'mysql', 'postgres', 등)
+  //   logging: console.log, // 쿼리 로깅 (필요에 따라 true/false 설정)
+  //   timezone: "+09:00",
+  // },
+  // production: {
+  //   username: process.env.DB_USERNAME,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_DATABASE,
+  //   host: process.env.DB_HOST,
+  //   dialect: process.env.DB_DIALECT,
+  //   logging: false, // production 환경에서는 쿼리 로깅을 끄는 것이 일반적
+  //   timezone: "+09:00",
+  // },
   development: {
-    username: process.env.DB_USERNAME, // .env에서 불러오기
-    password: process.env.DB_PASSWORD, // .env에서 불러오기
-    database: process.env.DB_DATABASE, // .env에서 불러오기
-    host: process.env.DB_HOST, // .env에서 불러오기
-    dialect: process.env.DB_DIALECT, // .env에서 불러오기 (예: 'mysql', 'postgres', 등)
-    logging: console.log, // 쿼리 로깅 (필요에 따라 true/false 설정)
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: console.log, // 개발 환경에서는 쿼리 로그를 활성화
     timezone: "+09:00",
   },
   production: {
@@ -15,8 +34,9 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    logging: false, // production 환경에서는 쿼리 로깅을 끄는 것이 일반적
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false, // 배포 환경에서는 쿼리 로그를 비활성화
     timezone: "+09:00",
   },
 };
