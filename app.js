857981e461a39ddfe8b2d59131006e5842aa3fcf
iyeonggyu0 <iyeonggyu0@gmail.com>
2025-01-24 02:09:56 +0900

    commit

--- app.js
+++ app.js
@@ -80,13 +80,13 @@ app.get("/", (req, res) => {
   res.status(200).json({
     status: "success",
     message: "Express Server is Running",
-    dbConfig: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      dialect: process.env.DB_DIALECT,
-    },
+    // dbConfig: {
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   host: process.env.DB_HOST,
+    //   dialect: process.env.DB_DIALECT,
+    // },
   });
 });
 
